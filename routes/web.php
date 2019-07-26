eec5d20205820aa8431fe04a6ace1941a19de03f
Jorge Sarmiento <jdavids991@gmail.com>
2019-07-26 16:07:11 -0400

    rutas agregadas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return 'hola';
+});
+
+Route::get('/usuario', function(){
+    return 'Usuarios';
+});
+
+// ruta con url amigable 
+Route::get('/usuario/{id}', function($id){
+    return "Mostrando detelle de usuario: {$id}";
+})->where ('id','[0-9]+');
+
+Route::get('/usuario/nuevo', function(){
+    return 'Crear Nuevo Usuario';
 });
